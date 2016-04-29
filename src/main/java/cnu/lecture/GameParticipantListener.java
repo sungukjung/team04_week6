259ec4c4f335a5cb21f10fe8870e73c8b016b7b1
ChoJunYoung <jjy891224@gmail.com>
2016-04-29 18:52:51 +0900

    test

--- src/main/java/cnu/lecture/GameParticipantListener.java
+++ src/main/java/cnu/lecture/GameParticipantListener.java
@@ -3,6 +3,8 @@ package cnu.lecture;
 /**
  * Created by tchi on 2016. 4. 25..
  */
-public interface GameParticipantListener {
-    void player(String summonerName);
-}
+
+
+//public interface GameParticipantListener {
+//    void player(String summonerName);
+//}
